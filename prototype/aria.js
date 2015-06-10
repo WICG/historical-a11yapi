af777341afcd44aedbc60941c4b13e96d3e8f75a
Marcos Caceres <marcos+github@marcosc.com>
2015-06-10 16:09:57 -0400

    Changed quotes to single quotes

--- prototype/aria.js
+++ prototype/aria.js
@@ -3,18 +3,18 @@
  */
 const ARIASemantics = {
   global: {
-    match: ":role",
+    match: ':role',
     name: [
-      ":attr(aria-label)",
-      ":idrefs(aria-labelledby)"
+      ':attr(aria-label)',
+      ':idrefs(aria-labelledby)'
     ],
     description: [
-      ":idrefs(aria-describedby)"
+      ':idrefs(aria-describedby)'
     ]
   },
   button: {
-    match: "[role='button']",
-    role: "button",
-    name: [":content"]
+    match: '[role="button"]',
+    role: 'button',
+    name: [':content']
   }
 };
